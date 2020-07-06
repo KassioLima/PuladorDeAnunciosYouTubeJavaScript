647917e748835f72c14cb3e7f40a6b82473a516a
kassio <kassio.lima.p@gmail.com>
2020-07-06 03:18:38 -0300

    Tentando resolver o problema das múltipas chamadas

--- background.js
+++ background.js
@@ -1,27 +1,64 @@
 // this is the background code...
-var controle = 0;
-var intervalo = setInterval(function(){}, 60*1000);
+function createCookie (name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+createCookie('pulador', null, null);
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab)
 {
-    if(controle === 0)
+    if(tab)
+    if(getCookie('pulador').split(",").indexOf(""+tabId) < 0)
     {
-        controle++;
-        console.log("Chamou "+controle);
-        // if(tab.url.includes("youtube.com"))
-        // {
-        //     // for the current tab, inject the "inject.js" file & execue it
-        //     chrome.tabs.executeScript(tab.ib,
-        //     {
-        //         file: 'inject.js'
-        //     });
-        // }
-    }
-    else
-    {
-        clearInterval(intervalo);
-        intervalo = setInterval(function ()
+        if (tab.url.includes("youtube.com"))
         {
-            controle = 0;
-        }, 2);
+            var a = getCookie('pulador').split(",");
+            var b = [tabId];
+            var c = a.concat(b);
+            var d = c.filter((item, pos) => c.indexOf(item) === pos);
+            createCookie('pulador', d, null);
+            // for the current tab, inject the "inject.js" file & execue it
+            chrome.tabs.executeScript(tab.ib,
+            {
+                file: 'inject.js'
+            }, function(result) {
+                    // Process |result| here (or maybe do nothing at all).
+                });
+        }
+        else if(!tab.url.includes("chrome://extensions/"))
+        {
+            var a = getCookie('pulador').split(",");
+            a.splice(a.indexOf(""+tabId));
+            createCookie('pulador', a, null);
+        }
     }
+});
+chrome.tabs.onRemoved.addListener(function (tabId, changeInfo, tab)
+{
+    var a = getCookie('pulador').split(",");
+    a.splice(a.indexOf(""+tabId));
+    createCookie('pulador', a, null);
 });
--- inject.js
+++ inject.js
@@ -1,30 +1,37 @@
 // this is the code which will be injected into a given page...
-(function()
+(function ()
 {
-	console.log("Código chamado");
-	var intervalo = setInterval(function()
-	{
-		//controla se vc tá vendo um vídeo ou não
-		if(window.location.href.includes("watch"))
+
+	// if(!window.location.href.includes('#pularanuncio'))
+	// {
+		var intervalo = setInterval(function ()
 		{
-			try
+			console.log("rodando");
+			//controla se vc tá vendo um vídeo ou não
+			if (window.location.href.includes("watch"))
 			{
-				try
-				{
-					//fecha aquele bannerzinho horizontal que aparece em cima do vídeo
-					document.getElementsByClassName("ytp-ad-overlay-close-button")[0].click();
-					console.log("Propaganda fechada ;)");
+				try {
+					try {
+						//fecha aquele bannerzinho horizontal que aparece em cima do vídeo
+						document.getElementsByClassName("ytp-ad-overlay-close-button")[0].click();
+						console.log("Propaganda fechada ;)");
+					} catch (err) {
+					}
+					//clica no botão de pular o anúncio
+					//funciona mesmo quando o botão ainda não apareceu
+					document.getElementsByClassName("ytp-ad-skip-button")[0].click();
+					console.log("Anúncio pulado bb");
+				} catch (err) {
 				}
-				catch(err)
-				{}
-				//clica no botão de pular o anúncio
-				//funciona mesmo quando o botão ainda não apareceu
-				document.getElementsByClassName("ytp-ad-skip-button")[0].click();
-				console.log("Anúncio pulado bb");
 			}
-			catch(err)
-			{}
-		}
-	}, 500);
+			//saiu do youtube na aba
+			if(!window.location.href.includes('youtube.com'))
+			{
+				console.log("parou");
+				clearInterval(intervalo);
+			}
+		}, 5000);
+	// }
+	// history.pushState('','', '#pularanuncio');
 })();
 
