5127152e25dedc52e0f5ecf7e7971d1f22b28978
kassio <kassio.lima.p@gmail.com>
2020-07-05 22:55:03 -0300

    Tentando resolver o problema das múltipas chamadas

--- background.js
+++ background.js
@@ -1,14 +1,12 @@
 // this is the background code...
 var controle = 0;
+var intervalo = setInterval(function(){}, 60*1000);
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab)
 {
-    if(parseInt(controle) === 0)
+    if(controle === 0)
     {
         controle++;
-        console.log("Change inifo");
-        console.log(changeInfo);
-        console.log("tab");
-        console.log(tab);
+        console.log("Chamou "+controle);
         // if(tab.url.includes("youtube.com"))
         // {
         //     // for the current tab, inject the "inject.js" file & execue it
@@ -17,5 +15,13 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab)
         //         file: 'inject.js'
         //     });
         // }
+    }
+    else
+    {
+        clearInterval(intervalo);
+        intervalo = setInterval(function ()
+        {
+            controle = 0;
+        }, 2);
     }
 });
